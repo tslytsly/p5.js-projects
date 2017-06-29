58f66478cd1719905c8621a94f692d9b83b140b0
Thomas Sealey <tpsealey@gmail.com>
2017-06-29 08:27:27 +0100

    added end detection

--- Sorting/sketch.js
+++ Sorting/sketch.js
@@ -1,14 +1,18 @@
 var sortSet = [];
-var sortSetLength = 100;
-var x = 6;
-
+var sortSetLength;
+var x = 10;
+var sorted = false;
+var swapMade = false;
 var swapPoint = 0;
+
 
 
 function setup() {
     console.log("hello");
     createCanvas(600, 400);
     background(51);
+
+    sortSetLength = width / x;
 
     for (i = 0; i < sortSetLength; i++) {
         sortSet[i] = i
@@ -26,20 +30,34 @@ function setup() {
 
 function draw() {
     background(51);
-    if (swapPoint > sortSet.length) swapPoint = 0;
+    if (swapPoint > sortSet.length && !swapMade) {
+        sorted = true;
+    } else if (swapPoint > sortSet.length) {
+        swapPoint = 0;
+        swapMade = false;
+    }
+
     var j = swapPoint;
     var k = swapPoint + 1;
-    if (sortSet[j] > sortSet[k]) {
-        var temp = sortSet[k]
-        sortSet[k] = sortSet[j];
-        sortSet[j] = temp;
+
+    if (!sorted) {
+        if (sortSet[j] > sortSet[k]) {
+            var temp = sortSet[k]
+            sortSet[k] = sortSet[j];
+            sortSet[j] = temp;
+            swapMade = true;
+        }
+        swapPoint++;
     }
-    swapPoint++;
 
     for (i = 0; i < sortSet.length; i++) {
         //normalise
         var h = height / sortSetLength * sortSet[i];
-        fill(255);
+        if (sorted) {
+            fill(0, 255, 0);
+        } else {
+            fill(255);
+        }
         rect(x * i, height - h, x, h);
     }
 }
