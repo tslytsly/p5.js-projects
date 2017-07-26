e6caf299a30fb8e13f982f3b9528dba10f1c12a6
tslytsly <tpsealey@gmail.com>
2017-07-26 13:23:58 +0100

    added more comments

--- BitwiseAnd/sketch.js
+++ BitwiseAnd/sketch.js
@@ -16,31 +16,39 @@ function setup() {
         portArr[i] = pow(2, portExp);
         portExp = portExp - 1;
     }
-
+    //main function
     portCheck(bitmask);
 
 }
 
 function portCheck(msk) {
-    //split into substrings of 8 char
+    //split the provided bitmask into substrings of 8 char
     mask = msk.match(/.{1,8}/g);
-    //convert them to a hex
+
+    // this will loop through all the masks, there will be
+    // 16 in total, every 2 is one switch, total of 8 possible
+    // in a single stack
+
     for (i = 0; i < mask.length; i++) {
         console.log("Start of Loop No. " + i);
+        // convert from string to hex num
         mask[i] = "0x" + mask[i];
         console.log("Mask " + i + " is " + mask[i]);
 
         //check them against the port array
+        // this is doing a bitand to every possible port
         for (j = 0; j < 32; j++) {
             resultArr[j + idx] = ((portArr[j + idx] & mask[i]) !== 0);
         }
-
-
+        // idx keeps track of where we are up to
         idx += 32;
         console.log("idx= " + idx);
 
         console.log("End of Loop No. " + i);
     }
+
+
+    // once we have checked all switches we need to report the state for each port
     for (i = 1; i < resultArr.length; i++) {
         console.log("Port " + i + " is in VLAN = " + resultArr[i]);
     }
